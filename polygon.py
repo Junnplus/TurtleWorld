f341d0bd6411512177713805717f55ff5947590e
nplus <nplus@localhost.localdomain>
2015-03-29 10:44:54 +0800

    generalization square

--- polygon.py
+++ polygon.py
@@ -6,3 +6,9 @@ def square(t, length):
     for i in range(4):
         fd(t, length)
         lt(t)
+
+def polygon(t, n, length):
+    angle = 360/n
+    for i in range(n):
+        fd(t, length)
+        lt(t, angle)
